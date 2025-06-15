a34e016c7d262e6caa8acbe539f60405ca89995a
ugnicholas <ugnicholas@gmail.com>
2025-06-15 12:53:40 +0000

    added customer entity

---
+++ OrderManagement.Domain/Entities/Customer/Customer.cs
@@ -0,0 +1,18 @@
+﻿using OrderManagement.Domain.Common;
+using OrderManagement.Domain.Enum;
+using OrderManagement.Domain.Entities.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Domain.Entities.Customer
+{
+    public class Customer: BaseEntity
+    {
+        public string Name { get; set; }
+        public CustomerSegment Segment { get; set; }
+        public ICollection<Order.Order> Orders { get; set; }
+    }
+}
