f341934355a1bd5109ceb1446828f888df2be30e
ugnicholas <ugnicholas@gmail.com>
2025-06-15 02:19:51 +0000

    added db context

---
+++ OrderManagement.Infrastructure/Data/OrderManagementDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.Domain.Entities.Order;
+using OrderManagement.Domain.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Infrastructure.Data
+{
+    public class OrderManagementDbContext: DbContext
+    {
+        public OrderManagementDbContext(DbContextOptions<OrderManagementDbContext> options): base(options) { }
+        
+        public DbSet<Product> Products => Set<Product>();
+
+        public DbSet<Order> Orders => Set<Order>();
+
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        public DbSet<OrderStatus> OrderStatus => Set<OrderStatus>();
+    }
+}
