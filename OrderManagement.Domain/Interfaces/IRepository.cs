6c4b34d245c725df23cb3d0b97f8e33ef0a4adf9
ugnicholas <ugnicholas@gmail.com>
2025-06-15 08:45:34 +0000

    created base repository

---
+++ OrderManagement.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task <IEnumerable<T>> GetAllAsync();
+        Task AddAsync (T entity);
+        Task DeleteAsync (Guid id);
+        Task UpdateAsync (T entity);
+
+    }
+}
