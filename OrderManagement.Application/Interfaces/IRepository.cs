c2473441417ff77aa66ed9eec27d3c3af24b779b
ugnicholas <ugnicholas@gmail.com>
2025-06-15 12:53:23 +0000

    added application layer interfaces

---
+++ OrderManagement.Application/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task <IEnumerable<T>> GetAllAsync();
+        Task AddAsync (T entity);
+        void Delete (T entity);
+        void Update (T entity);
+
+    }
+}
