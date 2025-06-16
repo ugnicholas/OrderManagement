0336baef24070ff60df0afd27d21125106d52fff
ugnicholas <ugnicholas@gmail.com>
2025-06-16 03:35:25 +0000

    register repos and services

--- OrderManagement.Infrastructure/DependancyInjection.cs
+++ OrderManagement.Infrastructure/DependancyInjection.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OrderManagement.Application.Interfaces;
 using OrderManagement.Infrastructure.Data;
+using OrderManagement.Infrastructure.Repositories;
+using OrderManagement.Infrastructure.Services;
 
 namespace OrderManagement.Infrastructure
 {
@@ -9,8 +12,18 @@ namespace OrderManagement.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
         {
-            
+            services.AddScoped<IDiscountService, DiscountService>();
 
+            // Generic base repository
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+            // Register repositories
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Unit of Work
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             return services;
         }
