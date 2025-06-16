2829765eac05d789c31d7f1e38f9d5091bb54452
ugnicholas <ugnicholas@gmail.com>
2025-06-16 00:01:21 +0000

    refactor

---
+++ OrderManagement.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Domain.Entities.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Infrastructure.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
+            builder.HasMany(o => o.Items)
+                   .WithOne()
+                   .HasForeignKey("OrderId");
+
+            builder.HasMany(o => o.StatusHistory)
+                   .WithOne()
+                   .HasForeignKey("OrderId");
+        }
+    }
+}
---
+++ OrderManagement.Infrastructure/DependancyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderManagement.Infrastructure.Data;
+
+namespace OrderManagement.Infrastructure
+{
+    public static class DependancyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+        {
+            
+
+
+            return services;
+        }
+    }
+}
