0d726b509de585ef32717c691b659e5614b182a7
ugnicholas <ugnicholas@gmail.com>
2025-06-16 00:00:51 +0000

    added jwt authentication

---
+++ OrderManagement.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace OrderManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            // Validate user here...
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "3f3179b2-a33b-4b99-ac28-35b7002adf75"),
+                new Claim(ClaimTypes.Email, request.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ordermgtsecret"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "OrderManagementApi",
+                audience: "OrderManagementClient",
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+    }
+}
