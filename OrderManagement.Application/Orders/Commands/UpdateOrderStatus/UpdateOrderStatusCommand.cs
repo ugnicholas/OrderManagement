920437070b21c497eb329024e81dd3b86d7f114d
ugnicholas <ugnicholas@gmail.com>
2025-06-16 03:26:15 +0000

    added command and query files

---
+++ OrderManagement.Application/Orders/Commands/UpdateOrderStatus/UpdateOrderStatusCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using OrderManagement.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Orders.Commands.UpdateOrderStatus
+{
+    public class UpdateOrderStatusCommand : IRequest<bool>
+    {
+        public Guid OrderId { get; set; }
+        public OrderStatus NewStatus { get; set; }
+
+        public UpdateOrderStatusCommand(Guid orderId, OrderStatus newStatus)
+        {
+            OrderId = orderId;
+            NewStatus = newStatus;
+        }
+    }
+}
---
+++ OrderManagement.Application/Orders/Dtos/OrderAnalyticsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Orders.Dtos
+{
+    public class OrderAnalyticsDto
+    {
+        public int TotalOrders { get; set; }
+        public decimal AverageOrderValue { get; set; }
+        public double AverageFulfillmentTimeInHours { get; set; }
+        public double AverageProcessingTimeInHours { get; set; }
+        public Dictionary<string, int> OrdersByStatus { get; set; } = new();
+    }
+}
